c1c8e154df4a8d5484c8b1d2cf300a7460e8d305
KABANGA David <Davkabanga96@gmail.com>
2023-09-01 09:33:37 +0200

    adding chabges

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
-
-
+import React, { useState, useEffect } from 'react'
+import { commerce } from "./lib/commerce";
 import { Products, Navbar } from "./components";
 
 const App = () => {
+
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
         <Navbar />
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,14 +3,20 @@ import Grid from "@material-ui/core/Grid";
 
 import Product from "./Product/Product";
 
+import useStyles from "./styles";
+
 const products = [
     {id: 1, name: "Shoes", description: "Sport Shoes", price: "$5", image: "https://media.istockphoto.com/id/1346094881/photo/cropped-shot-of-an-unrecognizable-woman-tying-her-shoelaces-while-exercising-at-the-gym.jpg?s=612x612&w=0&k=20&c=MFlCV5osWA0t7r2oTP6PTzvfKoO2NUDhF40zzkomxGc="},
     {id: 2, name: "Macbook", description: "Apple Macbook", price: "$10", image: "https://plus.unsplash.com/premium_photo-1681160405593-4f794e7e671d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWFjYm9vayUyMHByb3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"},
 ]
 
 const Products = () => {
+
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+        <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
