41b6c58c7f848a4ccb151d8860fe2041d44885a9
KABANGA David <Davkabanga96@gmail.com>
2023-09-07 17:00:33 +0200

    Displaying cart items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 
 import useStyles from "./styles";
+import CartItem from './CartItem/CartItem';
 
 const Cart = ({ cart }) => {
     
@@ -17,9 +18,7 @@ const Cart = ({ cart }) => {
             <Grid container spacig={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>
-                            {item.name}
-                        </div>
+                        <CartItem item={item} />
                     </Grid>
                 ))}
             </Grid>
