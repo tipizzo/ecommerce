42a06f3cd372cfc92abac3acee57fe5620ddf1ee
KABANGA David <Davkabanga96@gmail.com>
2023-09-20 12:25:18 +0200

    Fixing bugs in checkout component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { commerce } from "./lib/commerce";
-import { Products, Navbar, Cart, Checkout } from "./components";
+import { Products, Navbar, Cart, Checkout} from "./components";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
@@ -70,9 +70,7 @@ const App = () => {
                 } 
 
               />
-              <Route exact path='/checkout'>
-                  <Checkout />
-              </Route>
+              <Route exact path='/checkout' element={<Checkout />} />
             
           </Routes>
       </div>
